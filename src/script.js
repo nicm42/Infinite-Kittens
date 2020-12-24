9c8be787f519195856ca3b7fe3791a276abf3056
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-24 20:09:41 +0000

    Started setting up the cat api

--- src/script.js
+++ src/script.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+import { config } from "./config";
+const apiKey = config.apiKey;
+
+async function getCats() {
+  try {
+    //const cors = 'https://cors-anywhere.herokuapp.com/';
+    const link = 'https://thecatapi.com/v1/images/search?limit=5';
+    const request = await fetch(link, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': apiKey
+      },
+      mode: 'cors'
+    });
+    if(request.ok) {
+      const data = await request.json();
+      //const data = await request;
+      console.log(data);
+    } else {
+      throw new Error(request.statusText);
+    }
+  } catch (error) {
+    console.log(error);
+  }  
+}
+
+getCats();

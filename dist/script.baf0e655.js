39fd11b9727b882886730d33d350cd6dc38cf854
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-25 19:51:37 +0000

    Fetched 100 images and just shows 10

--- dist/script.baf0e655.js
+++ dist/script.baf0e655.js
@@ -118,10 +118,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/script.js":[function(require,module,exports) {
+const cats = document.querySelector('.cats');
+
 async function getCats() {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg,png';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
     const request = await fetch(link);
 
     if (request.ok) {
@@ -137,13 +139,24 @@ async function getCats() {
 }
 
 function showCats(data) {
-  //console.log(data[0].url);
-  const cats = document.querySelector('.cats');
-  data.forEach(cat => {
+  //Just want to add 10 of these for now - we'll add another 10 when the user scrolls
+  let catsCount = 0;
+  addCats(data, catsCount);
+  /* data.forEach(cat => {
     const image = document.createElement('img');
     image.setAttribute('src', cat.url);
     cats.appendChild(image);
-  });
+  }) */
+}
+
+function addCats(data, catsCount) {
+  for (let i = catsCount; i < catsCount + 10; i++) {
+    const image = document.createElement('img');
+    image.setAttribute('src', data[i].url);
+    cats.appendChild(image);
+  }
+
+  catsCount += 10;
 }
 
 getCats().then(data => {

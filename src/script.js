a7c94c5edd9a623c1300b271e70162807883298d
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-25 19:41:10 +0000

    Updated link to just fetch static images

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 async function getCats() {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=100';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
     const request = await fetch(link);
     if(request.ok) {
       const data = await request.json();
@@ -16,7 +16,7 @@ async function getCats() {
 }
 
 function showCats(data) {
-  console.log(data[0].url);
+  //console.log(data[0].url);
   const cats = document.querySelector('.cats');
   const image = document.createElement('img');
   image.setAttribute('src', data[0].url);

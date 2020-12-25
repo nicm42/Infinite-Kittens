cb2cf59886672d29f38937380b57bab1069664bc
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-25 19:45:32 +0000

    Added all 10 images it fetches to the page

--- dist/script.baf0e655.js
+++ dist/script.baf0e655.js
@@ -121,7 +121,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 async function getCats() {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg,png';
     const request = await fetch(link);
 
     if (request.ok) {
@@ -139,9 +139,11 @@ async function getCats() {
 function showCats(data) {
   //console.log(data[0].url);
   const cats = document.querySelector('.cats');
-  const image = document.createElement('img');
-  image.setAttribute('src', data[0].url);
-  cats.appendChild(image);
+  data.forEach(cat => {
+    const image = document.createElement('img');
+    image.setAttribute('src', cat.url);
+    cats.appendChild(image);
+  });
 }
 
 getCats().then(data => {
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 async function getCats() {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg,png';
     const request = await fetch(link);
     if(request.ok) {
       const data = await request.json();
@@ -18,9 +18,11 @@ async function getCats() {
 function showCats(data) {
   //console.log(data[0].url);
   const cats = document.querySelector('.cats');
-  const image = document.createElement('img');
-  image.setAttribute('src', data[0].url);
-  cats.appendChild(image);
+  data.forEach(cat => {
+    const image = document.createElement('img');
+    image.setAttribute('src', cat.url);
+    cats.appendChild(image);
+  })
 }
 
 getCats()

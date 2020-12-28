84531ec33e404f5715cc9064e13e7677731a062b
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-28 20:06:47 +0000

    Added the basics of the intersection observer

--- dist/script.baf0e655.js
+++ dist/script.baf0e655.js
@@ -120,16 +120,17 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"src/script.js":[function(require,module,exports) {
 const cats = document.querySelector('.cats');
 
-async function getCats() {
+async function getCats(limit) {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=' + limit + '&mime_types=jpg,png';
     const request = await fetch(link);
 
     if (request.ok) {
-      const data = await request.json(); //console.log(data);
+      const data = await request.json();
+      console.log(data); //return data;
 
-      return data;
+      showCats(data);
     } else {
       throw new Error(request.statusText);
     }
@@ -140,28 +141,27 @@ async function getCats() {
 
 function showCats(data) {
   //Just want to add 10 of these for now - we'll add another 10 when the user scrolls
-  let catsCount = 0;
-  addCats(data, catsCount);
-  /* data.forEach(cat => {
+  //let catsCount = 0;
+  //addCats(data, catsCount);
+  data.forEach(cat => {
     const image = document.createElement('img');
     image.setAttribute('src', cat.url);
     cats.appendChild(image);
-  }) */
-}
+  });
+} //function addCats(data, catsCount) {
 
-function addCats(data, catsCount) {
-  for (let i = catsCount; i < catsCount + 10; i++) {
-    const image = document.createElement('img');
-    image.setAttribute('src', data[i].url);
-    cats.appendChild(image);
-  }
+/* for(let i = catsCount; i < catsCount + 10; i++) {
+  const image = document.createElement('img');
+  image.setAttribute('src', data[i].url);
+  cats.appendChild(image);    
+} */
 
-  catsCount += 10;
+/* catsCount += 10;
+if(catsCount >= data.length) {
+  fisherYates(data);
+} */
+//}
 
-  if (catsCount >= data.length) {
-    fisherYates(data);
-  }
-}
 
 function fisherYates(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -172,11 +172,24 @@ function fisherYates(array) {
   }
 }
 
-getCats().then(data => {
-  if (data) {
+const observerOptions = {};
+const catsObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(entry => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      //Since it's intersecting on page load, we don't need to run getCats outside of here
+      getCats(10);
+    }
+  });
+}, observerOptions);
+catsObserver.observe(cats); //getCats(10)
+
+/* .then(data => {
+  if(data) {
     showCats(data);
   }
-});
+}); */
 },{}],"../../../../usr/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -205,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36789" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46287" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
 const cats = document.querySelector('.cats');
 
-async function getCats() {
+async function getCats(limit) {
   try {
     //This needs a number for limit, otherwise it will only fetch one image
-    const link = 'https://api.thecatapi.com/v1/images/search?limit=100&mime_types=jpg,png';
+    const link = 'https://api.thecatapi.com/v1/images/search?limit=' + limit + '&mime_types=jpg,png';
     const request = await fetch(link);
     if(request.ok) {
       const data = await request.json();
-      //console.log(data);
-      return data;
+      console.log(data);
+      //return data;
+      showCats(data);
     } else {
       throw new Error(request.statusText);
     }
@@ -19,26 +20,26 @@ async function getCats() {
 
 function showCats(data) {
   //Just want to add 10 of these for now - we'll add another 10 when the user scrolls
-  let catsCount = 0;
-  addCats(data, catsCount);
-  /* data.forEach(cat => {
+  //let catsCount = 0;
+  //addCats(data, catsCount);
+  data.forEach(cat => {
     const image = document.createElement('img');
     image.setAttribute('src', cat.url);
     cats.appendChild(image);
-  }) */
+  })
 }
 
-function addCats(data, catsCount) {
-  for(let i = catsCount; i < catsCount + 10; i++) {
+//function addCats(data, catsCount) {
+  /* for(let i = catsCount; i < catsCount + 10; i++) {
     const image = document.createElement('img');
     image.setAttribute('src', data[i].url);
     cats.appendChild(image);    
-  }
-  catsCount += 10;
+  } */
+  /* catsCount += 10;
   if(catsCount >= data.length) {
     fisherYates(data);
-  }
-}
+  } */
+//}
 
 function fisherYates(array) {
   for(let i = array.length - 1; i > 0; i--){
@@ -49,9 +50,25 @@ function fisherYates(array) {
   }
 }
 
-getCats()
-  .then(data => {
+const observerOptions = {
+
+}
+
+const catsObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+      //Since it's intersecting on page load, we don't need to run getCats outside of here
+      getCats(10);
+    }
+  })
+}, observerOptions)
+
+catsObserver.observe(cats);
+
+//getCats(10)
+  /* .then(data => {
     if(data) {
       showCats(data);
     }
-  });
+  }); */
